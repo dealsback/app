5d207146a28cd624899069df3d4e137bc8bec451
dealsback <dealsback.in@gmail.com>
2026-09-13 14:38:25 +0530

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,45 @@
+const CACHE_NAME = 'dealsback-pwa-v1';
+const ASSETS_TO_CACHE = [
+  '/',
+  '/BazaarGo.html',
+  '/manifest.json',
+  '/dealsback.png',
+  '/favicon.png',
+  '/thumb.jpg'
+];
+
+// Service Worker Installation
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS_TO_CACHE).catch(() => {});
+    })
+  );
+  self.skipWaiting();
+});
+
+// Service Worker Activation & Cache Cleanup
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) => {
+      return Promise.all(
+        keys.map((key) => {
+          if (key !== CACHE_NAME) {
+            return caches.delete(key);
+          }
+        })
+      );
+    })
+  );
+  self.clients.claim();
+});
+
+// Fetch Intercepting
+self.addEventListener('fetch', (event) => {
+  if (event.request.method !== 'GET') return;
+  event.respondWith(
+    fetch(event.request).catch(() => {
+      return caches.match(event.request);
+    })
+  );
+});
